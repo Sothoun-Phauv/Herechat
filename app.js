aa536639a73c64724848ca7c1214ff4965544ac9
Student <student@PNCLB12-09.pnc.lan>
2021-05-20 14:25:55 +0700

    yet

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello world!!');
+const express = require("express");
+const app = express();
+const SERVER_PORT = 3000;
+
+app.use(express.static("public"));
+
+app.listen(SERVER_PORT, function () {
+    console.log("listening on : " + SERVER_PORT);
+});
---
+++ js/script.js
@@ -0,0 +1,3 @@
+const SERVER_PORT = 3000;
+const SERVER_IP = "192.168.88.31";
+const BASE_URL = "http://" + SERVER_IP + ":" + SERVER_PORT + "/score";
